e2c5c39e3bd5d25c6b42f56cf92f313b901ef147
hactazia <hactazia@gmail.com>
2025-12-05 18:07:11 +0000

    feat: adding fix blendshape rendering

--- src/lib/openapi.ts
+++ src/lib/openapi.ts
@@ -2,7 +2,7 @@ import { createOpenAPI } from 'fumadocs-openapi/server';
 
 export const options = {
   input: ['./public/openapi.yaml'],
-  output: './content/docs/api/(generated)',
+  output: './content/api/(generated)',
 };
 
 export const openapi = createOpenAPI({
